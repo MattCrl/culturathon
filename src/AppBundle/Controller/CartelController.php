ff3de0de3f470cc57bd97f97fbd2dc6312f9bc51
Matt Crl <matth.carlevaris@gmail.com>
2018-06-14 23:12:04 +0200

    Cartel page done

---
+++ src/AppBundle/Controller/CartelController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mattcrl
+ * Date: 14/06/18
+ * Time: 19:05
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Cartel;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartelController extends Controller
+{
+
+    /**
+     * @param Cartel $cartel
+     *
+     * @Route("cartel/{id}", name="cartel")
+     * @Method("GET")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showAction(Cartel $cartel)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $cartels = $em->getRepository('AppBundle:Cartel')->findAll();
+
+        return $this->render('cartel/cartel.html.twig', [
+            'cartel' => $cartel,
+            'cartels' => $cartels,
+        ]);
+    }
+
+}
